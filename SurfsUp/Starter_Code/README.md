2863f02161aff97e6833ad81364157b8ed460b9e
jbelmar1 <jo.belmar@hotmail.com>
2024-04-03 19:32:38 -0500

    Module 10 Assignment

---
+++ SurfsUp/Starter_Code/README.md
@@ -0,0 +1,16 @@
+Module 10 Challenge
+
+Congratulations! You've decided to treat yourself to a long holiday vacation in Honolulu, Hawaii. To help with your trip planning, you decide to do a climate analysis about the area.
+
+
+# Part 1: Analyze and Explore the Climate Data
+In this section, you’ll use Python and SQLAlchemy to do a basic climate analysis and data exploration of your climate database
+* Precipitation Analysis
+* Station Analysis
+* List the manager of each department along with their department number, department name, employee number, last name, and first name.
+* List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+* List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+* List each employee in the Sales department, including their employee number, last name, and first name.
+
+# Part 2: Design Your Climate App
+* Now that you’ve completed your initial analysis, you’ll design a Flask API based on the queries that you just developed.
---
+++ SurfsUp/Starter_Code/Starter_Code/app.py
@@ -0,0 +1,135 @@
+# Import the dependencies.
+import numpy as np
+import pandas as pd
+import sqlalchemy
+import datetime as dt
+import re
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+#1
+@app.route("/")
+def welcome():
+"""List all available api routes."""
+return (
+        f"Welcome to the SQL-Alchemy APP API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
+    )
+
+#2
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    recent_year= dt.date(2017, 8, 23)-dt.timedelta(days=365)
+    last_date = dt.date(recent_year.year, recent_year.month, recent_year.day)
+    query= session.query(measurement.date, measurement.prcp).filter(measurement.date >= last_date).order_by(measurement.date).all()
+    prcp_dict = dict(query)
+
+    print(f"Precipitation Results - {prcp_dict}")
+    print("Out of Precipitation section.")
+    return jsonify(prcp_dict) 
+
+
+prcp_dates = []
+prcp_totals = []
+
+for date, dailytotal in precipitation:
+    prcp_dates.append(date)
+    prcp_totals.append(dailytotal)
+    
+    prcp_dict = dict(zip(prcp_dates, prcp_totals))
+
+return jsonify(prcp_dict)
+
+#3
+@app.route("/api/v1.0/stations")
+def stations():
+    stations_query = session.query(Station.name, Station.station)
+    stations = pd.read_sql(stations_query.statement, stations_query.session.bind)
+return jsonify(stations.to_dict())
+
+#4
+def tobs():
+session = Session(engine)
+queryresult = session.query( Measurement.date, Measurement.tobs).filter(Measurement.station=='USC00519281')\
+     .filter(Measurement.date>='2016-08-23').all()
+
+tob = []
+for date, tobs in queryresult:
+    tobs_dict = {}
+    tobs_dict["Date"] = date
+    tobs_dict["Tobs"] = tobs
+    tob_obs.append(tobs_dict)
+return jsonify(tob)
+
+#5 start
+@app.route("/api/v1.0/<start>")
+
+def temps_start(start):
+    session = Session(engine)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+              filter(Measurement.date >= start).all()
+    session.close()
+
+tobs_values = []
+for min, avg, max in results:
+    tobs_dict = {}
+    tobs_dict["min_temp"] = min
+    tobs_dict["avg_temp"] = avg
+    tobs_dict["max_temp"] = max
+    tobs.append(tobs_dict) 
+return jsonify(tobs_values)
+
+#5 end
+@app.route("/api/v1.0/<start>/<end>")
+def temps_end(start, end):
+    session = Session(engine)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+              filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    session.close()
+
+tobs_values = []
+for min, avg, max in results:
+    tobs_dict = {}
+    tobs_dict["min_temp"] = min
+    tobs_dict["avg_temp"] = avg
+    tobs_dict["max_temp"] = max
+    tobs.append(tobs_dict) 
+return jsonify(tobs_values)
+
+if __name__ == "__main__":
+    app.run(debug=True)
